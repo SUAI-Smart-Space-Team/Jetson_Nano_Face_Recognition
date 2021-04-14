151fa064692a8a7d26cd03d5d69293e4604076db
Fedor Scherbachenko <59050463+FrostFrost1380@users.noreply.github.com>
2021-04-15 00:25:01 +0300

    Create cve.sh

---
+++ jetson_api/cve.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+curl https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sh && \
+
+    ./bin/trivy fs -s CRITICAL --exit-code 1 / && \
+
+    ./bin/trivy fs -s HIGH / && \
+
+    ./bin/trivy image --reset && \
+
+    rm ./bin/trivy
+   
