1915feda90cf20d42a947198636295978f5499e7
Valentin Kovalev <59050499+Kovixer@users.noreply.github.com>
2021-04-15 02:14:31 +0300

    Update cve.sh

--- jetson_api/cve.sh
+++ jetson_api/cve.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sh && \
+wget https://raw.githubusercontent.com/aquasecurity/trivy/master/contrib/install.sh | sh && \
 
     ./bin/trivy fs -s CRITICAL --exit-code 1 / && \
 
