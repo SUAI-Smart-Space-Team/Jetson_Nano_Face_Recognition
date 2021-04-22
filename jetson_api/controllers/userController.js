356db88306a61236b6463914132d7df4294a1b4d
Valentin Kovalev <59050499+Kovixer@users.noreply.github.com>
2021-04-22 20:36:04 +0300

    Update userController.js

--- jetson_api/controllers/userController.js
+++ jetson_api/controllers/userController.js
@@ -24,6 +24,7 @@ const userController = async (req, res, next) => {
         })
     } else {
         fs.readdir(tmpPath, (err, files) => {
+            console.log(files)
             if (files.length === 0) return
             files.forEach((file) => {
                 const objectId = new mongoose.Types.ObjectId()
