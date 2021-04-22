d80b7835ef5283f290160437e57dc87e2cd1033f
Valentin Kovalev <59050499+Kovixer@users.noreply.github.com>
2021-04-22 20:15:21 +0300

    Update userController.js

--- jetson_api/controllers/userController.js
+++ jetson_api/controllers/userController.js
@@ -7,7 +7,7 @@ const Image = require('../models/Image')
 const Stat = require('../models/Stat')
 
 const tmpPath = path.normalize(`${__dirname}/../../jetson_tmp/`)
-
+console.log(`tmpPath = ${tmpPath}`)
 const userController = async (req, res, next) => {
    
     const { id: name } = JSON.parse(req.body);
